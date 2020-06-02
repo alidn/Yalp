086733731d847791d4588c9640e0f2ba6026e583
ali <anili@my.bcit.ca>
2020-06-02 00:58:30 -0700

    Added a few unit tests for the round-robin balancer

---
+++ balancer/roundrobin_test.go
@@ -0,0 +1,63 @@
+package balancer
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestNextBackend(t *testing.T) {
+	testServer1 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		println("Got a request for server 1")
+	}))
+
+	testServer2 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		println("Got a request for server 1")
+	}))
+
+	testServer3 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		println("Got a request for server 1")
+	}))
+
+	t.Logf(testServer1.URL, testServer2.URL, testServer3.URL)
+
+	deadServerURL1 := "http://localhost:1111"
+	deadServerURL2 := "http://localhost:2222"
+
+	roundRobinBalancer, err := NewRoundRobinBalancerWithURLs(testServer1.URL, deadServerURL1, testServer2.URL, deadServerURL2, testServer3.URL)
+	if err != nil {
+		t.Error(err)
+	}
+
+	nextBackend, err := roundRobinBalancer.NextBackend()
+	if err != nil {
+		t.Error("ERR!", err)
+	}
+	if roundRobinBalancer.GetCurIndex() != 0 {
+		t.Errorf("index wrong, got %d, expected %d", roundRobinBalancer.GetCurIndex(), 0)
+	}
+	if nextBackend.Addr != testServer1.URL {
+		t.Errorf("first server url is wrong, got %s, expected %s", nextBackend.Addr, testServer1.URL)
+	}
+	println("finished first")
+
+	nextBackend, err = roundRobinBalancer.NextBackend()
+	if err != nil {
+		t.Error("ERR!", err)
+	}
+	if nextBackend.Addr != testServer2.URL {
+		t.Errorf("second server url is wrong, got %s, expected %s", nextBackend.Addr, testServer2.URL)
+	}
+	println("finished second")
+
+	testServer3.Close()
+	nextBackend, err = roundRobinBalancer.NextBackend()
+	if err != nil {
+		t.Error("ERR!", err)
+	}
+	if nextBackend.Addr != testServer1.URL {
+		t.Errorf("third server url is wrong, got %s, expected %s", nextBackend.Addr, testServer1.URL)
+	}
+	println("finished third")
+
+}
