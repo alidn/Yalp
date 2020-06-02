27f82cc6490ecc187661204e6b2cd70003c9e809
ali <anili@my.bcit.ca>
2020-06-02 01:13:45 -0700

    Fixed a bug in the health-check, and improevd error handling

--- backend/backend.go
+++ backend/backend.go
@@ -1,24 +1,47 @@
 package backend
 
 import (
+	"errors"
 	"net"
+	"net/url"
 	"time"
 )
 
+// Backend represents a backend server.
 type Backend struct {
-	addr string
-	IsAlive bool
+	Addr string
+	// shows whether or not the server is alive.
+	IsAlive           bool
 	healthCheckTicker *time.Ticker
 }
 
-func NewBackend(addr string) *Backend {
-	return &Backend{
-		addr: addr,
-		IsAlive: false,
+func NewBackend(addr string) (*Backend, error) {
+	backend := &Backend{
+		Addr: addr,
+		// TODO: having a default value might cause errors
+		IsAlive: true,
 	}
+	isAlive, err := backend.CheckAlive()
+	if err != nil {
+		backend.IsAlive = false
+	} else {
+		backend.IsAlive = isAlive
+	}
+
+	// TODO: find out why a new goroutine is absolutely necessary here.
+	go func() {
+		err = backend.StartHealthCheck()
+	}()
+
+	if err != nil {
+		return nil, err
+	}
+	return backend, nil
 }
 
-func (b Backend) StartHealthCheck() error {
+// StartHealthCheck starts the healt-check which checks if the backend
+// is alive every second.
+func (b *Backend) StartHealthCheck() error {
 	// TODO: decide a better duration or use a config file
 	b.healthCheckTicker = time.NewTicker(time.Second)
 	for {
@@ -34,15 +57,26 @@ func (b Backend) StartHealthCheck() error {
 	}
 }
 
-func (b Backend) StopHealthCheck() {
+// StopHealthCheck stops the health-check ticker.
+func (b *Backend) StopHealthCheck() {
 	b.healthCheckTicker.Stop()
 }
 
-func (b Backend) CheckAlive() (bool, error) {
-	// TODO: find a better timeout, or maybe use a config file
-	conn, err := net.DialTimeout("tcp", b.addr, time.Second*2)
+// CheckAlive checks if the backend is still alive using a TCP connection
+// with a timeout of 2 seconds. It returns an error if the TCP connection
+// fails.
+func (b *Backend) CheckAlive() (bool, error) {
+	parsedURL, err := url.Parse(b.Addr)
 	if err != nil {
-		return false, err
+		return false, errors.New("could not parser url: %s: %s" + b.Addr + " : " + err.Error())
+	}
+	tcpURL := parsedURL.Host
+
+	// TODO: find a better timeout, or maybe use a config file
+	conn, err := net.DialTimeout("tcp", tcpURL, time.Second)
+	if err != nil {
+		return false, nil
+		// return false, errors.New("could not do a tcp dial: " + err.Error())
 	}
 
 	err = conn.Close()
--- backend/backendpool.go
+++ backend/backendpool.go
@@ -1,6 +1,28 @@
 package backend
 
 type BackendPool struct {
-	Backends          []*Backend
+	Backends []*Backend
 }
 
+func NewBackendPool() *BackendPool {
+	return &BackendPool{
+		Backends: make([]*Backend, 0),
+	}
+}
+
+// NewBackendPoolFromURLs constructs and returns a new BackendPool using the
+// given urls.
+func NewBackendPoolFromURLs(urls ...string) (*BackendPool, error) {
+	backendPool := &BackendPool{
+		Backends: make([]*Backend, 0),
+	}
+	for _, url := range urls {
+		backend, err := NewBackend(url)
+		if err != nil {
+			return nil, err
+		}
+		backendPool.Backends = append(backendPool.Backends, backend)
+	}
+
+	return backendPool, nil
+}
