0a3ad95fad1882f3fb4d3be50123e5dd6b5a537c
ali <anili@my.bcit.ca>
2020-06-08 17:34:35 -0700

    Added more unit tests

--- balancer/roundrobin_test.go
+++ balancer/roundrobin_test.go
@@ -3,6 +3,7 @@ package balancer
 import (
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
 	"time"
 )
@@ -106,4 +107,125 @@ func TestNextBackend1(t *testing.T) {
 	if nextBackend.Addr != testServer2.URL {
 		t.Errorf("third server url is wrong, got %s, expected %s", nextBackend.Addr, testServer2.URL)
 	}
+}
+
+func TestServerProxy(t *testing.T) {
+	resSeq := make([]int, 0)
+	server1 := newTestServer(&resSeq, 1)
+	server2 := newTestServer(&resSeq, 2)
+	server3 := newTestServer(&resSeq, 3)
+
+	balancer, err := NewRoundRobinBalancerWithURLs(server1.URL, server2.URL, server3.URL)
+	if err != nil {
+		t.Error("Could not get the balancer", err)
+	}
+
+	frontendProxy := httptest.NewServer(balancer.NewReverseProxy())
+	defer frontendProxy.Close()
+
+	for i := 0; len(resSeq) < 10; i++ {
+		_, err = http.Get(frontendProxy.URL)
+		if err != nil {
+			t.Error("Could not get the reponse", err)
+		}
+	}
+
+	expected := []int{1, 2, 3, 1, 2, 3, 1, 2, 3, 1}
+	if !reflect.DeepEqual(expected, resSeq) {
+		t.Error("The sequence of servers isn't the same")
+	}
+
+	server1.Close()
+	server2.Close()
+	server3.Close()
+}
+
+func TestServerProxyWithFailure(t *testing.T) {
+	resSeq := make([]int, 0)
+	server1 := newTestServer(&resSeq, 1)
+	server2 := newTestServer(&resSeq, 2)
+	server3 := newTestServer(&resSeq, 3)
+
+	balancer, err := NewRoundRobinBalancerWithURLs(server1.URL, server2.URL, server3.URL)
+	if err != nil {
+		t.Error("Could not get the balancer", err)
+	}
+
+	frontendProxy := httptest.NewServer(balancer.NewReverseProxy())
+	defer frontendProxy.Close()
+
+	for i := 0; len(resSeq) < 10; i++ {
+		if i == 6 {
+			server2.Close()
+		}
+		_, err = http.Get(frontendProxy.URL)
+		if err != nil {
+			t.Error("Could not get the reponse", err)
+		}
+	}
+
+	expected := []int{1, 2, 3, 1, 2, 3, 1, 3, 1, 3}
+	if !reflect.DeepEqual(expected, resSeq) {
+		t.Errorf("The sequence of servers isn't the same\n, got %#v \n, expected %#v", resSeq, expected)
+	}
+
+	server1.Close()
+	server3.Close()
+}
+
+func TestServerProxyWithFailure2(t *testing.T) {
+	resSeq := make([]int, 0)
+	server1 := newTestServer(&resSeq, 1)
+	server2 := newTestServer(&resSeq, 2)
+	server3 := newTestServer(&resSeq, 3)
+
+	balancer, err := NewRoundRobinBalancerWithURLs(server1.URL, server2.URL, server3.URL)
+	if err != nil {
+		t.Error("Could not get the balancer", err)
+	}
+
+	frontendProxy := httptest.NewServer(balancer.NewReverseProxy())
+	defer frontendProxy.Close()
+
+	for i := 0; len(resSeq) < 10; i++ {
+		if i == 2 {
+			server3.Close()
+		}
+		_, err = http.Get(frontendProxy.URL)
+		if err != nil {
+			t.Error("Could not get the reponse", err)
+		}
+	}
+
+	expected := []int{1, 2, 1, 2, 1, 2, 1, 2, 1, 2}
+	if !reflect.DeepEqual(expected, resSeq) {
+		t.Errorf("The sequence of servers isn't the same\n, got %#v \n, expected %#v", resSeq, expected)
+	}
+
+	server1.Close()
+	server3.Close()
+}
+
+func TestServerProxyWithNoAliveBackend(t *testing.T) {
+	balancer, err := NewRoundRobinBalancerWithURLs("http://127.0.0.1:00010")
+	if err != nil {
+		t.Error("Could not get the balancer", err)
+	}
+
+	time.Sleep(2 * time.Second)
+
+	backend, err := balancer.NextBackend()
+	if backend != nil {
+		t.Errorf("Backend should be nil, got %+v", backend)
+	}
+	if err.Error() != "none of the servers is alive" {
+		t.Errorf("Got the wrong error")
+	}
+}
+
+func newTestServer(resSeq *[]int, id int) *httptest.Server {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		*resSeq = append(*resSeq, id)
+	}))
+	return server
 }
